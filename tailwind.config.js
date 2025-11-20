6c2bd12fa2acaab64bacfb39e75418e0ffdcef16
Dauren Nurgaziyev <d.k.nurgaziev@gmail.com>
2025-11-20 22:13:20 +0500

    1/3 from category

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["/index.html"],
+  theme: {
+    extend: {
+      content: {
+        link: 'url("../images/home.png")',
+      },
+      spacing: {
+        "icon-size": "1.25rem", // Например, 20px
+      },
+    },
+  },
+  plugins: [],
+};
