df6d41a806f35baed00f12b5f9642de3e7065037
Dauren <d.k.nurgaziev@gmail.com>
2025-11-27 16:26:35 +0500

    load folder with files

--- js/addSong.js
+++ js/addSong.js
@@ -1,11 +1,41 @@
 const audioFile = document.querySelector("#audio-file");
 const player = document.querySelector("#player");
 const songs = document.querySelector(".songs");
+const songsCount = document.querySelector("#songsCount");
+
+function updateCount() {
+  const count = Number(localStorage.getItem("songsCount")) || 0;
+  songsCount.textContent = `${count} Songs`;
+}
+
+updateCount();
 
 audioFile.addEventListener("change", function (e) {
-  const file = this.files[0];
-  if (!file) return;
-  const link = URL.createObjectURL(file);
-  console.log(file.name);
-  songs.innerHTML = `<audio src="${link}"></audio>`;
+  const files = e.target.files;
+  for (const file of files) {
+    console.log("name", file.name);
+    let performer = prompt(`Название альбома ${file.name}`);
+    console.log("performer", performer);
+    const date = new Date(file.lastModified)
+      .toLocaleDateString()
+      .replaceAll("/", ".");
+    console.log("date", date);
+  }
+  songsCount.innerHTML = "";
+  console.log(files.length);
+  localStorage.setItem("songsCount", files.length);
+  updateCount();
 });
+
+function addMusic(name, performer, date) {
+  songs.innerHTML = `
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-2 p-4 text-white text-[20px]">
+      <div>${name}</div>
+      <div>${date}</div>
+      <div>${performer}</div>
+      <div>${date}</div>
+    </div>
+  `;
+}
+
+addMusic();
